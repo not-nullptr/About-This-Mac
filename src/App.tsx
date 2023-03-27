54d09be3b829d956d891b2fcd58b8760bb6baf71
madeline.xoxo <needhelpwithrift@gmail.com>
2023-03-27 10:48:25 +0100

    more boot disk support

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,16 @@ import './OSX.css';
 import apple from './apple.png'
 import * as si from 'systeminformation';
 import { useEffect, useState } from 'react';
-import * as util from 'util';
+import util from 'util';
 import { exec } from 'child_process';
 
-function nearestPowerOf2(num: number) {
-  return Math.pow(2, Math.round(Math.log(num) / Math.log(2)));
-}
-function titleCase(str: string) {
-  var splitStr = str.toLowerCase().split(' ');
-  for (var i = 0; i < splitStr.length; i++) {
-    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-  }
-  return splitStr.join(' ');
-}
 function App() {
   const [speed, setSpeed] = useState<string>()
   const [model, setModel] = useState<string>()
   const [gpu, setGPU] = useState<string>()
   const [osinfo, setOsInfo] = useState<si.Systeminformation.OsData>();
   const [std, setStd] = useState<string>();
+  const execPromise = util.promisify(exec)
   useEffect(() => {
     si.cpu().then(cpu => {
       setSpeed(`${cpu.speedMax} GHz`)
@@ -28,13 +19,25 @@ function App() {
     });
     si.graphics().then(gpus => {
       const gpu = gpus.controllers[0];
-      setGPU(`${Math.round(gpu.vram! / 1000)} GB ${gpu.name}`)
+      setGPU(`${Math.round(gpu.vram! / 1000)} GB ${gpu.model.includes("Intel") ? gpu.model : gpu.name}`)
     });
     si.osInfo().then(osinfo => {
       setOsInfo(osinfo);
     });
-    exec(`awk '$2 == "/"' /proc/self/mounts`, (err, stdout, stderr) => {
-      setStd(stdout.split(' ')[0]);
+    let command = "echo Unknown";
+    switch (osinfo?.platform) {
+      case "linux":
+        command = `awk '$2 == "/"' /proc/self/mounts`
+        break;
+      case "darwin":
+        command = `diskutil info / | sed -n 's/^ *Volume Name: *//p'`
+        break;
+      default:
+        command = "echo Unknown"
+    }
+    execPromise(command).then((value) => {
+      setStd(value.stdout);
+      console.log(value.stdout)
     })
   }, [])
   return (
@@ -65,7 +68,19 @@ function App() {
               }
             }
           })()}</div>
-          <div id="subtitle">Version {osinfo?.kernel}</div>
+          <div id="subtitle">Version {(() => {
+            switch (osinfo?.platform) {
+              case "linux": {
+                return osinfo?.kernel;
+              }
+              case "darwin": {
+                return osinfo?.release;
+              }
+              case "windows": {
+                // todo: make
+              }
+            }
+          })()}</div>
           <button onClick={() => {
             exec('/usr/bin/kitty /usr/bin/bash -c \"sudo pacman -Syu; read -n 1 -s -r -p \\\"Done! Press any key to continue.\\\"\"')
           }}>Software Update...</button>
@@ -88,9 +103,11 @@ function App() {
           <button style={{ marginTop: -10 }} onClick={() => {
             exec('/usr/bin/kitty /usr/bin/bash -c \"neofetch; read -n 1 -s -r -p \\\"Press any key to continue.\\\"\"')
           }}>More Info...</button>
-          <div id="footer">
-            {`TM and © 1983-${(new Date()).getFullYear()} madzzz <33`} <br />
-            All Rights Reserved. {'(but not really)'}
+          <div id="footer-container">
+            <div id="footer">
+              {`TM and © 1983-${(new Date()).getFullYear()} madzzz <33`} <br />
+              All Rights Reserved. {'(but not really)'}
+            </div>
           </div>
         </div>
       </div>
